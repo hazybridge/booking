2e1fca6034ed7f86fcd8d94e4d91b54ef5b6a0e1
Hazy Bridge <126696802+hazybridge@users.noreply.github.com>
2024-07-26 10:33:44 -0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-
 import { RouterView } from 'vue-router'
-import { supabase } from './lib/supabaseClient'
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-  console.log('data', data)
-}
-onMounted(() => {
-  getCountries()
-})
 </script>
 
 <template>
